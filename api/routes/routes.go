b29321328a81efa43b9d58c2a379e2e22a33aa82
GitHub xkiven <GitHub 2674302433@qq.com>
2025-02-07 16:55:06 +0800

    登录最后检验验证码并生成token

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -3,10 +3,12 @@ package routes
 import (
 	"Rope_Net/api/handlers/user_handlers"
 	"Rope_Net/api/handlers/user_handlers/login"
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(r *gin.Engine) {
+	r.Use(sessions.Sessions("mySession", sessions.NewCookieStore([]byte("secret"))))
 	r.Group("/api")
 	{
 		user := r.Group("/user")

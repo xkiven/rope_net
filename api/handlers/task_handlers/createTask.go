eda0de55f0a8d7393adc8bfe96530252f00c574d
GitHub xkiven <GitHub 2674302433@qq.com>
2025-02-11 21:39:13 +0800

    修改参数时间的绑定

--- api/handlers/task_handlers/createTask.go
+++ api/handlers/task_handlers/createTask.go
@@ -10,6 +10,7 @@ import (
 )
 
 func CreateTask(c *gin.Context) {
+	var task models.Task
 	// 从上下文中获取用户信息
 	logger.Info("从上下文获取用户信息")
 	user, exists := c.Get("user")
@@ -21,9 +22,12 @@ func CreateTask(c *gin.Context) {
 	}
 	currentUser := user.(*models.User)
 	//绑定参数
+	var newTask struct {
+		Name     string `json:"name"`
+		Deadline string `json:"deadline"`
+	}
 	logger.Info("绑定参数")
-	var task models.Task
-	if err := c.ShouldBind(&task); err != nil {
+	if err := c.ShouldBind(&newTask); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"status": 10001,
 			"info":   "绑定参数失败",
@@ -31,8 +35,19 @@ func CreateTask(c *gin.Context) {
 		logger.Info(err)
 		return
 	}
+	deadline, err := time.Parse(time.RFC3339, newTask.Deadline)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status": 10013,
+			"error":  "无效的截止时间格式，使用 RFC3339 格式 (YYYY-MM-DDTHH:MM:SSZ)",
+		})
+		logger.Error(err)
+		return
+	}
+	//创建任务
 	task.UserID = currentUser.Id
-	task.Deadline = time.Now()
+	task.Deadline = deadline
+	task.Name = newTask.Name
 	//连接数据库
 	db, err := db2.ConnectDB()
 	if err != nil {
@@ -58,7 +73,7 @@ func CreateTask(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{
 		"status": 10000,
-		"info":   "创建任务失败",
+		"info":   "创建任务成功",
 	})
 
 }
